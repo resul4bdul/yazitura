dbf2ad6b8f175d96cab1c59fc84d334e7fde3621
Resul4bdul <145356280+resul4bdul@users.noreply.github.com>
2023-10-22 17:54:41 +0400

    Add files via upload

---
+++ yazitura.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import random
+from gtts import gTTS
+import os
+
+def yazı_tura_çevir():
+    cavab = random.choice(["Yazı", "Tura"])
+    return cavab
+
+
+def cavabi_sesli_soyle(cavab):
+    tts = gTTS(f"{cavab}", lang='tr') 
+    tts.save("cavab.mp3")
+    os.system("mpg123 cavab.mp3")  
+
+if __name__ == "__main__":
+    input("Yazı-tura atmağ üçün 'ENTER' düyməsinə basın...")
+    cavab = yazı_tura_çevir()
+    cavabi_sesli_soyle(cavab)
+
